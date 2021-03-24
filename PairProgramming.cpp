6947e1e2e52116ca72b5eba6c33acf029fac7376
Dustin Osweiler <dosweiler@carthage.edu>
2021-03-24 14:42:06 -0500

    Created file and made Classes

---
+++ PairProgramming.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Point {
+public:
+	double x;
+	double y;
+};
+
+class Line {
+public:
+	double x1;
+	double y1;
+	double x2;
+	double y2;
+};
+
+int main() {
+
+}

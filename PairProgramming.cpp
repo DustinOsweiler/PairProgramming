b118360e98b8e432cbeff6af97598419c35dbe27
Dustin Osweiler <dosweiler@carthage.edu>
2021-03-31 14:36:22 -0500

    Update PairProgramming.cpp

--- PairProgramming.cpp
+++ PairProgramming.cpp
@@ -1,20 +1,53 @@
 #include <iostream>
-using namespace std;
 
+<<<<<<< Updated upstream
+	using namespace std;
+#define CATCH_CONFIG_MAIN
+
+=======
 class Point {
+private:
+	int x, y;
+
 public:
-	double x;
-	double y;
+	Point() : x(0), y(0) {}
+	Point(double x, double y) : x(x), y(y) {}
 };
 
 class Line {
+	private: 
+	Point p1;
+	Point p2;
+
 public:
-	double x1;
-	double y1;
-	double x2;
-	double y2;
+	Line::Line()
+	{
+	}
+
+	Line(Point start, Point end) : p1(start), p2(end)
+	{		
+	}
+
+	const Point &startPoint() const{
+		return p1;
+	}
+	Point &startPoint()
+	{
+		return p1;
+	}
+	const Point & endPoint() const 
+	{
+		return p2;
+	}
+	Point &endPoint()
+	{
+		return p2;
+	}
 };
 
 int main() {
-
+	Line line;
+	line.startPoint() = Point(4, 2);
+	line.endPoint() = Point(6, 9);
 }
+>>>>>>> Stashed changes

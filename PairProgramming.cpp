331ecf34af2dde0f21b5f92d37cd10aaa4689f82
Dustin Osweiler <dosweiler@carthage.edu>
2021-03-31 22:22:44 -0500

    Added the Parallel and GetSlope Function

--- PairProgramming.cpp
+++ PairProgramming.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
-<<<<<<< Updated upstream
+<<<<<< <Updated upstream
 	using namespace std;
 #define CATCH_CONFIG_MAIN
 
-=======
+
 class Point {
 private:
 	int x, y;
@@ -12,10 +12,12 @@ private:
 public:
 	Point() : x(0), y(0) {}
 	Point(double x, double y) : x(x), y(y) {}
+	int& GetX() { return x; }
+	int& GetY() { return y; }
 };
 
 class Line {
-	private: 
+private:
 	Point p1;
 	Point p2;
 
@@ -25,29 +27,48 @@ public:
 	}
 
 	Line(Point start, Point end) : p1(start), p2(end)
-	{		
+	{
 	}
 
-	const Point &startPoint() const{
+	const Point& startPoint() const {
 		return p1;
 	}
-	Point &startPoint()
+	Point& startPoint()
 	{
 		return p1;
 	}
-	const Point & endPoint() const 
+	const Point& endPoint() const
 	{
 		return p2;
 	}
-	Point &endPoint()
+	Point& endPoint()
 	{
 		return p2;
 	}
 };
+
+double GetSlope(Line l1) {
+	double Y = l1.endPoint().GetY() - l1.startPoint().GetY();
+	double X = l1.endPoint().GetX() - l1.startPoint().GetX();
+	double LineSlope = Y / X;
+}
+
+void Parallel(Line l1, Line l2) {
+	double Slope1 = GetSlope(l1);
+	double Slope2 = GetSlope(l2);
+	if (Slope1 == Slope2)
+		cout << "The two lines are parallel";
+	else
+		cout << "The two lines are not parallel";
+}
+
+void GetLength(Line) {
+
+}
 
 int main() {
 	Line line;
 	line.startPoint() = Point(4, 2);
 	line.endPoint() = Point(6, 9);
 }
->>>>>>> Stashed changes
+>>>>>> > Stashed changes
